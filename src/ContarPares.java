9093bb74838db4a3b22ea72a5254f0c7a723a78d
entornosDaw19 <45696683+entornosDaw19@users.noreply.github.com>
2020-03-04 13:54:03 +0100

    Update ContarPares.java

--- src/ContarPares.java
+++ src/ContarPares.java
@@ -11,7 +11,9 @@ public static void main(String[] args) {
 int contadorPares = calculaNumPareshastaN(10);
 System.out.println("Hay "+contadorPares+" números de pares del 0 al 10");
 System.out.println("FIn del programa");
-System.out.println("FIn del programa2");		
+System.out.println("FIn del programa2");	
+	
+System.out.println("FIn del programa3 DESDE GITHUB !!");
 }
 
 private static int calculaNumPareshastaN(int n) {
